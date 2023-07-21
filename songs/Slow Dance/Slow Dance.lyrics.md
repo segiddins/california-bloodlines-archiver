f9afd29143e91ba97f9ac3895d50b8036898edc2
Samuel Giddins <segiddins@segiddins.me>
2023-07-21 14:10:05 -0700

    Generate chordpro files

--- songs/Slow Dance/Slow Dance.lyrics.md
+++ songs/Slow Dance/Slow Dance.lyrics.md
@@ -1,1 +1,27 @@
-Instrumental Track
+Green eyes and summer moonlight
+Blue moon in a summer's sky
+The future in moment's glances
+Heaven in a lover's eye
+
+Friday falls like warm moonbeam
+Monday falls and the rain is cold
+And Friday falls like sweet sunshine
+Saturday falls like gold 
+
+Chorus:
+For a slow dance in a blue blue moon
+A slow dance as the river runs
+One chance came none to soon
+For a slow dance in a blue blue moon
+
+Music's like a midnight railroad
+Electric as a dance hall band
+Slow dance is a blues in moonlight
+Moonlight is a lover's hand
+
+Dreaming in blue blue shadows
+Holding someone four four time
+Dancing in a million shadows
+Dancing in a lover's eye
+
+(Repeat Chorus)
--- writeSongMd.go
+++ writeSongMd.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
-	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
@@ -32,7 +32,7 @@ func (m writeSongMdModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case songMdWrittenMsg:
 		m.written++
 		cmd = m.progress.SetPercent(float64(m.written) / float64(m.toWrite))
-		return m, cmd
+		return m, tea.Batch(cmd)
 	case progress.FrameMsg:
 		prog, cmd := m.progress.Update(msg)
 		m.progress = prog.(progress.Model)
@@ -52,13 +52,16 @@ func (m writeSongMdModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View implements tea.Model.
 func (m writeSongMdModel) View() string {
+	if m.toWrite <= m.written {
+		return "Done!"
+	}
 	return "Writing songs...\n" + m.progress.View() + fmt.Sprintf("\n%d/%d", m.written, m.toWrite)
 }
 
 func writeSongMd(songs map[int]*song) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 
-	for idx, song := range songs {
+	for _, song := range songs {
 		song := song
 		song.lock.RLock()
 		defer song.lock.RUnlock()
@@ -74,11 +77,8 @@ func writeSongMd(songs map[int]*song) (tea.Model, tea.Cmd) {
 		}
 
 		dir := filepath.Join("songs", title)
-		dur := time.Duration(idx) * 20 * time.Millisecond
 
 		cmds = append(cmds, func() tea.Msg {
-			time.Sleep(dur)
-			_ = dir
 			if err := os.MkdirAll(dir, 0755); err != nil {
 				return errMsg{err}
 			}
@@ -101,12 +101,37 @@ func writeSongMd(songs map[int]*song) (tea.Model, tea.Cmd) {
 			}
 
 			return songMdWrittenMsg{}
+		}, func() tea.Msg {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				return errMsg{err}
+			}
+
+			tab := strings.TrimSpace(song.Attrs["Tab"]) + "\n"
+			if len(tab) <= 1 {
+				return songMdWrittenMsg{}
+			}
+
+			pat := regexp.MustCompile(`\(([A-G].*?)\)`)
+			tab = pat.ReplaceAllString(tab, `[$1]`)
+
+			tab = regexp.MustCompile(`\[([^A-G].+?)\]`).ReplaceAllString(tab, `[*$1]`)
+
+			tab = fmt.Sprintf(`{title: %s}
+{artist: John Stewart}
+{composer: %s}
+{album: %s}
+
+`, title, song.Attrs["Songwriter"], song.Attrs["Recording"]) + tab
+
+			os.WriteFile(filepath.Join(dir, title+".chordpro"), []byte(tab), 0644)
+
+			return songMdWrittenMsg{}
 		})
 	}
 
 	return writeSongMdModel{
 		progress: progress.NewModel(),
-		toWrite:  len(songs),
+		toWrite:  len(songs) * 2,
 	}, tea.Batch(cmds...)
 }
 
